857e451d912c4b6c287bdc0e82e05fa9344a0dfc
moroccomicrosoftcommunity <mormicrosoftcom@gmail.com>
2024-09-06 13:40:42 +0100

    Add files via upload

---
+++ MMC_Params-master/ParamsService.Domain/DTOs/ParticipantsByEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParamsService.Domain.DTOs
+{
+    public class ParticipantsByEvent
+    {
+        public Guid Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Gender { get; set; }
+        public string? City { get; set; }
+        public bool ? Is_Participated { get; set; }
+    }
+}
